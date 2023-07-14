2f4bccaabe3503fd68fc0dbd47c3665ed1b94675
dongsam-E <dong2dong32@gmail.com>
2023-07-14 09:45:44 +0900

    메인브런치 첫작업

---
+++ php/page/way.php
@@ -0,0 +1,6 @@
+
+        <?php include_once("../top.php"); ?>
+        <div style="width:1200px; margin: 0 auto;">
+                찾아오시는 길 페이지입니다.
+        </div>
+        <?php include_once("../bottom.php"); ?>
---
+++ php/top.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> 서버언어들의 원리 </title>
+    <style>
+        body{
+            text-align:center;
+        }
+        header, footer{
+            background-color:black;
+            color:white;            
+        }
+        section{
+            height: 100vh;
+        }
+        aside{
+            position: fixed;
+            right:0;
+            bottom:0; 
+            background:yellow          
+        }
+    </style>
+</head>
+<body >
+    <header>
+        <ul>
+            <li><a href="/php/page/com.php">회사소개</a></li>
+            <li><a href="/php/page/way.php">오시는길</a></li>
+            <li><a href="/php/page/cart.php">장바구니</a></li>
+            <li><a href="/php/">홈</a></li>
+        </ul>
+    </header>
+    <section >
